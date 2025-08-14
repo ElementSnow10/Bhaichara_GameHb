33555f6596fd604e11eb6d7249c4d0c611b85a12
AnveshSPradhan <cs24b067@smail.iitm.ac.in>
2025-08-14 14:45:02 +0530

    update(2)

---
+++ Flappy_Bird/firebase-config.js
@@ -0,0 +1,13 @@
+// Firebase config for compat SDK usage. Loaded before game.js.
+// If you rotate credentials, update them here.
+
+window.firebaseConfig = {
+  apiKey: "AIzaSyCd6MiGb-o_eOwJM2d_UfuRTfdiRQBv4wY",
+  authDomain: "flappybirdleaderboard-7e1e0.firebaseapp.com",
+  projectId: "flappybirdleaderboard-7e1e0",
+  storageBucket: "flappybirdleaderboard-7e1e0.firebasestorage.app",
+  messagingSenderId: "115720973969",
+  appId: "1:115720973969:web:6cfa0af6e3917e84941930"
+};
+
+
